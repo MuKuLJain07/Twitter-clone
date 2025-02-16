278e44a6f5af634b4df4baa96827fc77dbe02bfa
Mukul Jain <mukuljain@ip-192-168-1-10.ap-south-1.compute.internal>
2025-02-16 19:46:50 +0530

    initial commit

---
+++ basicTwitter/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for basicTwitter project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicTwitter.settings')
+
+application = get_asgi_application()
